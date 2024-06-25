61218ad3348cb98d3e79d19f16d660a3a5b482c5
limoh653 <limohesbon7@gmail.com>
2024-06-25 20:47:57 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser +'!';
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+const excitedWelcomeMessage= 'WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + '!';
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting= `Welcome, ${currentUser.slice(0, 1)}!`;
